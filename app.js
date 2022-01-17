2f3b78594ebd8a5fc380f5a4dd2ddc97355b974e
OdinZD <odin.perica@gmail.com>
2022-01-17 13:37:19 +0100

    Update
    Add sounds, change sound.

--- app.js
+++ app.js
@@ -1,37 +1,107 @@
-class DrumKit{
-    constructor(){
-        this.pads = document.querySelectorAll('.pad');
-        this.playBtn = document.querySelector('.play');
-        this.kickAudio = document.querySelector('.kick-sound');
-        this.snareAudio = document.querySelector('.snare-sound');
-        this.hihatAudio = document.querySelector('.hihat-sound');
+class DrumKit {
+    constructor() {
+        this.pads = document.querySelectorAll(".pad");
+        this.playBtn = document.querySelector(".play");
+        this.currentKick = "./sounds/kick-classic.wav";
+        this.currentSnare = "./sounds/snare-acoustic01.wav";
+        this.currentHihat = "./sounds/hihat.acoustic01.wav";
+        this.kickAudio = document.querySelector(".kick-sound");
+        this.snareAudio = document.querySelector(".snare-sound");
+        this.hihatAudio = document.querySelector(".hihat-sound");
         this.index = 0;
         this.bpm = 150;
+        this.isPlaying = null;
+        this.selects = document.querySelectorAll("select");
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    //Loop over the pads
+    activeBars.forEach(bar => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      if (bar.classList.contains("active")) {
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    //Check if it's playing
+
+    if (this.isPlaying) {
+      //Clear the interval
+      clearInterval(this.isPlaying);
+      console.log(this.isPlaying);
+      this.isPlaying = null;
+    } else {
+      this.isPlaying = setInterval(() => {
+        this.repeat();
+      }, interval);
     }
-    activePad(){
-         this.classList.toggle('active');
+  }
+  updateBtn() {
+    //NULL
+
+    if (!this.isPlaying) {
+      this.playBtn.innerText = "STOP";
+      this.playBtn.classList.add("active");
+    } else {
+      this.playBtn.innerText = "Play";
+      this.playBtn.classList.remove("active");
     }
-    repeat(){
-        let step = this.index % 8;
-        const activeBars = document.querySelectorAll(`.b${step}`);
-        
-        this.index++;
+  }
+  changeSound(e) {
+    const selectionName = e.target.name;
+    const selectionValue = e.target.value;
+    switch (selectionName) {
+      case "kick-select":
+        this.kickAudio.src = selectionValue;
+        break;
+      case "snare-select":
+        this.snareAudio.src = selectionValue;
+        break;
+      case "hihat-select":
+        this.hihatAudio.src = selectionValue;
+        break;
     }
-    start(){
-        console.log(this);
-        const interval = (60/this.bpm) * 1000;
-        setInterval(() => {
-            this.repeat();
-        }, interval)
-    }
+  }
+  
+ 
 }
 
-
 const drumKit = new DrumKit();
-drumKit.pads.forEach(pad => {
-    pad.addEventListener('click', drumKit.activePad);
-})
 
-drumKit.playBtn.addEventListener('click', function() {
-    drumKit.start();
-})
+//Event Listeners
+
+drumKit.pads.forEach(pad => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function() {
+    this.style.animation = "";
+  });
+});
+
+drumKit.playBtn.addEventListener("click", function() {
+  drumKit.updateBtn();
+  drumKit.start();
+});
+
+drumKit.selects.forEach(select => {
+  select.addEventListener("change", function(e) {
+    drumKit.changeSound(e);
+  });
+});
